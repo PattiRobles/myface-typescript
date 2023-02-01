8bd3cc53c1651a8e54df6a019bdaf6f98528c9ef
Patricia Robles <patriciarobles.tech@gmail.com>
2023-02-01 10:27:58 +0000

    changes to CSS and start of DOM manipulation

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Add your JavaScript here.
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Add your JavaScript here.
+// });
+const menuButton = document.getElementById("menu_button");
+menuButton.addEventListener('click', function () {
+  console.log("hello, its working!")
+  console.log(menuButton)
+  // document.getElementsByClassName("menu").removeAttribute("display")
+})
